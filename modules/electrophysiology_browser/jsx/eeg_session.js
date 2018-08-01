bb75f3c3af3a75e61cd1eeb3ec149b1d828dd95f
Alizée <intralizee@users.noreply.github.com>
2018-08-01 16:44:24 -0400

    update

--- modules/electrophysiology_browser/jsx/eeg_session.js
+++ modules/electrophysiology_browser/jsx/eeg_session.js
@@ -1,34 +1,63 @@
 /**
- * This is the React class for the new_profile.
+ * This is the React class for the eeg_session.
  */
 class EEGSession extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      url: {
+        params: {
+          sessionID: '',
+          outputType: ''
+        }
+      }
 
     };
 
-
     // Bind component instance to custom methods
     this.fetchData = this.fetchData.bind(this);
-
+    this.collectParams = this.collectParams.bind(this);
   }
 
   /**
    * Fetch data when component mounts.
    */
   componentDidMount() {
+    this.collectParams();
     this.fetchData();
+  }
+
+  /**
+   * Post-Render when we can access the DOM.
+   */
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+
+  /**
+   * Retrieve params from the browser URL and save it in state.
+   */
+  collectParams() {
+    const url = new URL(window.location.href);
+    const outputType = url.searchParams.get('outputType');
+    const params = {
+      sessionID: url.searchParams.get('sessionID'),
+      outputType: outputType === null ? 'all_types' : outputType
+    };
+    this.state.url.params = params;
+    console.log(JSON.stringify(params));
   }
 
   /**
    * Retrieve data from the provided URL and save it in state.
    */
   fetchData() {
+
     $.ajax(loris.BaseURL + '/electrophysiology_browser/ajax/get_eeg_session_data.php', {
       method: 'GET',
       dataType: 'json',
+      data: this.state.url.params,
       success: function(data) {
         console.log('ajax (get) - success!');
         console.log(JSON.stringify(data));
@@ -36,7 +65,6 @@ class EEGSession extends React.Component {
       error: function(error) {
         console.log('ajax (get) - error!');
         console.log(JSON.stringify(error));
-
       }
     });
   }
@@ -46,9 +74,10 @@ class EEGSession extends React.Component {
    */
   render() {
 
+
     return (
       <div>
-        <div id="lorisworkspace">
+        <div id='lorisworkspace'>
           Hello
         </div>
       </div>
@@ -73,7 +102,7 @@ window.onload = function() {
   );
   // Create a wrapper div in which react component will be loaded.
   const EEGSessionAppDOM = document.createElement('div');
-  EEGSessionAppDOM.id = 'newProfile';
+  EEGSessionAppDOM.id = 'eegSession';
 
   // Append wrapper div to page content.
   const rootDOM = document.getElementById('lorisworkspace');
